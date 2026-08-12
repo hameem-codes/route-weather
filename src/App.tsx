2a558a8de8f8896fa85428b0f67a6da322b00911
Mateen <mateen.hameem@gmail.com>
2026-08-13 00:23:08 +0530

    Fix map canvas collapsing in production

--- src/App.tsx
+++ src/App.tsx
@@ -93,6 +93,7 @@ function App() {
             zoom: 7.5,
             pitch: 45
           }}
+          style={{ width: '100%', height: '100%' }}
           mapStyle="https://basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json"
         >
           {/* Glowing Route Line */}
